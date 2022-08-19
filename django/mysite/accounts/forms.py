36f33ec57bb538a2c878a789917a9b2061d3a4fb
Satyam0204 <satyampattanaik007@gmail.com>
2022-08-19 12:07:24 +0530

    successfully dockerized

--- django/mysite/accounts/forms.py
+++ django/mysite/accounts/forms.py
@@ -1,7 +1,7 @@
 
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
-from django import forms
+
 from django.contrib.auth.models import User
 
 from accounts.models import Book, Category, Order, Review, customerQuery
